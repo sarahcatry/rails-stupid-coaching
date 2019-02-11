480d54339a68b45c382654f06128fb0adf09c9e0
Sarah Catry <catry.sarah@gmail.com>
2019-02-11 15:02:27 +0100

    to do: 4 - Question Form Page

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @ask = params[:question]
+    if @ask[-1] == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @ask == 'I am going to work'
+      @answer = 'Great!'
+    else @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
